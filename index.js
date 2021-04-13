f79ded707f70f8dc7a637f67a9b39e36c8af3fcd
dhruvinfo28 <dhruv.info28@gmail.com>
2021-04-14 01:21:44 +0530

    Logging functionality

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
 const app = express();
+
+app.use(morgan('dev'));
+
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);

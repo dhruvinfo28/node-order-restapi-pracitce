440d3610713494c7d1b7749f39d119f6d493a33a
dhruvinfo28 <dhruv.info28@gmail.com>
2021-04-14 16:45:00 +0530

    Error handling

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
 
+//If any requests doesnt get handled by the above middlewares, this middleware below will listen for all other requests and 
+//create a new Error saying the url not found and set the status code to 404
+//Then the next() middleware is called along with the error as an argument, and if we have an error 404 that status code will be
+//set or if not a request related error 500 status code
+app.use((req,res,next)=>{
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            //Here is the error message, can be a Not found error or other . DB related errors , Usually errors have a message property
+            message: error.message
+        }
+    })
+})
+
 app.listen(process.env.PORT|| 3000, ()=>{
     console.log("listening")
 });
